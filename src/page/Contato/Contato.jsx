3fad151df39b7ac7bd5b7a33d9d3de78dfad847c
jonatasvnascimento <jonatasv.nascimento@gmail.com>
2021-05-19 17:11:47 -0300

    Config Contatos

--- src/page/Contato/Contato.jsx
+++ src/page/Contato/Contato.jsx
@@ -1,7 +1,28 @@
 import React from 'react'
+import { Header, Footer } from '../../components/index'
 
-export default function Contato(){
-    return(
-        <div>Page of contactos</div>
+export default function Contato() {
+    return (
+        <div>
+            <Header></Header>
+            <div className="container mt-3">
+                <form>
+                    <div className="form-group">
+                        <label>Nome Completo</label>
+                        <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="Nome Completo" />
+                    </div>
+                    <div className="form-group">
+                        <label >Endereço de email</label>
+                        <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="nome@exemplo.com" />
+                    </div>
+
+                    <div className="form-group">
+                        <label >Solicitação</label>
+                        <textarea className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                    </div>
+                </form>
+            </div>
+            <Footer></Footer>
+        </div>
     )
 }
