0830fbd4492242b22cff933f2b7f9d7197a15366
jonatasvnascimento <jonatasv.nascimento@gmail.com>
2022-01-30 18:58:52 -0300

    dev: layout do header refeito com map

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,64 +1,10 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
-import './style/Header.css';
-import {
-  Collapse,
-  Navbar,
-  NavbarToggler,
-  NavbarBrand,
-  Nav,
-  NavItem,
-  NavLink,
-  UncontrolledDropdown,
-  DropdownToggle,
-  DropdownMenu,
-  DropdownItem,
-  NavbarText,
-} from 'reactstrap';
+import React from 'react';
+import { Navbar } from './navbar/Navbar';
 
-const Header = (props) => {
-  const [isOpen, setIsOpen] = useState(false);
-
-  const toggle = () => setIsOpen(!isOpen);
-
+export default function Header() {
   return (
-    <div>
-      <Navbar expand="md" className="fixed-top NavbarDark ">
-        <div className="container">
-          <NavbarToggler onClick={toggle} />
-
-          <Collapse isOpen={isOpen} navbar className="positionLinks">
-            <Link to="/contato" className="LogoGhostBit">
-              GhostBit
-            </Link>
-            <Nav className="mr-auto" navbar>
-              <NavItem>
-                <Link to="/contato" className="styleLinks ">
-                  Notícias
-                </Link>
-              </NavItem>
-              <NavItem>
-                <Link to="/contato" className="styleLinks">
-                  Projetos
-                </Link>
-              </NavItem>
-              <NavItem>
-                <Link to="/contato" className="styleLinks">
-                  Portifólio
-                </Link>
-              </NavItem>
-              <NavItem>
-                <Link to="/contato" className="styleLinks">
-                  Contato
-                </Link>
-              </NavItem>
-            </Nav>
-            {/* <NavbarText>Simple Text</NavbarText> */}
-          </Collapse>
-        </div>
-      </Navbar>
-    </div>
+    <header>
+      <Navbar></Navbar>
+    </header>
   );
-};
-
-export default Header;
+}
---
+++ src/components/Header/navbar/MenuItems.jsx
@@ -0,0 +1,27 @@
+export const MenuItems = [
+  {
+    title: 'Home',
+    url: '/',
+    cName: 'nav-links',
+  },
+  {
+    title: 'Noticias',
+    url: '/noticias',
+    cName: 'nav-links',
+  },
+  {
+    title: 'Tools',
+    url: '/tools',
+    cName: 'nav-links',
+  },
+  {
+    title: 'Imposto de Renda',
+    url: '/News',
+    cName: 'nav-links',
+  },
+  {
+    title: 'API',
+    url: '/News',
+    cName: 'nav-links',
+  },
+];
