b11535b38097c40e57e2340b60746f6cf0504c11
Diego Brandao <diego_brandao@outlook.com.br>
2022-01-30 19:03:37 -0300

    dev: adicionado logo ghostbit

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -9,33 +9,35 @@ export default function Footer() {
         <footer className="pt-4 my-md-5 pt-md-5 border-top ">
             <div className="row">
                 <div className="col-12 col-md">
-                    <img className="mb-2" src="/docs/5.0/assets/brand/bootstrap-logo.svg" alt="" width="24" height="19" />
-                    <small className="d-block mb-3 text-muted">&copy; 2017–2022</small>
+                    <img className="mb-2" src="../../../ativo17.png" alt="" />
+                    <small className="d-block mb-3">&copy; 2017–2022</small>
                 </div>
                 <div className="col-6 col-md">
                     <h5>Redes Sociais</h5>
                     <ul className="list-unstyled text-small">
-                        <li className="mb-1"> <i class="fab fa-instagram"></i> <a href='https://www.instagram.com/diegobf_/' className="link-secondary text-decoration-none">Instagram</a></li>
-                        <li className="mb-1"> <i class="fab fa-whatsapp"></i> <a href='https://api.whatsapp.com/send?phone=5511910732978&text=Gostaria%20de%20Saber%20Mais' className="link-secondary text-decoration-none">Whatsapp</a></li>
-                        <li className="mb-1"> <i class="fab fa-twitch"></i> <a to="#" className="link-secondary text-decoration-none">Twitch</a></li>
-                        <li className="mb-1"> <i class="fab fa-twitter"></i> <a to="#" className="link-secondary text-decoration-none">Twitter</a></li>
+                        <li className="mb-1">  <a href='https://www.instagram.com/diegobf_/' className="link-secondary text-decoration-none"><i class="fab fa-instagram"></i> Instagram</a></li>
+                        <li className="mb-1">  <a href='https://api.whatsapp.com/send?phone=5511910732978&text=Gostaria%20de%20Saber%20Mais' className="link-secondary text-decoration-none"><i class="fab fa-whatsapp"></i> Whatsapp</a></li>
+                        <li className="mb-1">  <a to="#" className="link-secondary text-decoration-none"><i class="fab fa-twitch"></i> Twitch</a></li>
+                        <li className="mb-1">  <a to="#" className="link-secondary text-decoration-none"><i class="fab fa-twitter"></i> Twitter</a></li>
                     </ul>
                 </div>
                 <div className="col-6 col-md">
-                    <h5>Resources</h5>
+                    <h5>Principais</h5>
                     <ul className="list-unstyled text-small">
-                        <li className="mb-1"><Link to="#"className="link-secondary text-decoration-none" href="#">Resource</Link></li>
-                        <li className="mb-1"><Link to="#"className="link-secondary text-decoration-none" href="#">Resource name</Link></li>
-                        <li className="mb-1"><Link to="#"className="link-secondary text-decoration-none" href="#">Another resource</Link></li>
-                        <li className="mb-1"><Link to="#"className="link-secondary text-decoration-none" href="#">Final resource</Link></li>
+                        <li className="mb-1"><Link to="#"className="link-secondary text-decoration-none" href="#">Home</Link></li>
+                        <li className="mb-1"><Link to="#"className="link-secondary text-decoration-none" href="#">Serviços</Link></li>
+                        <li className="mb-1"><Link to="#"className="link-secondary text-decoration-none" href="#">Projetos</Link></li>
+                        <li className="mb-1"><Link to="#"className="link-secondary text-decoration-none" href="#">Contato</Link></li>
                     </ul>
                 </div>
                 <div className="col-6 col-md">
                     <h5>informações para Contato</h5>
                     <ul className="list-unstyled text-small">
-                        <li className="mb-1"><i class="fas fa-phone-alt"></i>(11) 9 1073-2978</li>
-                        <li className="mb-1"><i class="fal fa-envelope"></i> Email: dev.ghostbit@outlook.com</li>
-                       
+                        <br/>
+                        <li className="mb-1"><i class="fas fa-phone-alt"></i><span> 11 91073-2978</span></li>
+                        <li className="mb-1"><i class="fas fa-envelope"></i><span> Email: dev.ghostbit@outlook.com</span></li>
+                        <li className="mb-1"><i class="fas fa-envelope"></i> <span>07914080 Jardim Primavera. Francisco Morato SP</span></li>
+                        <br></br>
                     </ul>
                 </div>
             </div>
